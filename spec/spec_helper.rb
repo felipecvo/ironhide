fef3dd2378822c4a9dd2ff4e5ab00dceb48a2b2b
Felipe Oliveira <felipe.oliveira@abril.com.br>
2011-02-16 15:04:04 -0200

    Change tests to work without rails dependency, loading it before bundler, and working with rails 2.3.x and rails 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,19 @@
 require 'rubygems'
+
+begin
+  # rails 3.0.x
+  require 'rails'
+rescue LoadError
+  # rails 2.3.x
+  require 'initializer'
+end
+require 'action_view'
+require 'action_view/base'
+require 'action_view/helpers'
+require 'action_view/paths'
+require 'active_support/cache'
+require 'active_support/cache/memory_store'
+
 require 'bundler/setup'
 
 require 'ironhide'
@@ -6,6 +21,11 @@ require 'ironhide'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
-  Class.new(Rails::Application)
-  Rails.application.initializers.select { |x| x.name == :initialize_cache }.first.run
+  if Rails.version.first == '3'
+    Class.new(Rails::Application)
+    Rails.application.initializers.select { |x| x.name == :initialize_cache }.first.run
+  else
+    RAILS_ROOT = "./"
+    Rails::Initializer.new(Rails::Configuration.new).initialize_cache
+  end
 end
